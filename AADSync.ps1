268d2dfdaa80b81413f48d887c95803a06a21147
Travis <Scine@users.noreply.github.com>
2019-02-22 15:46:10 -0800

    Create AADSync.ps1

---
+++ AADSync.ps1
@@ -0,0 +1,8 @@
+##This forces AD Connect sync to kick off from Active Directory to Office 365.  Assuming you have AD Connect set up on one of your servers.
+##change FQDN on line 4 to the server's FQDN name.
+
+$AADComputer = "FQDN"
+$session = New-PSSession -ComputerName $AADComputer
+Invoke-Command -Session $session -ScriptBlock {Import-Module -Name 'ADSync'}
+Invoke-Command -Session $session -ScriptBlock {Start-ADSyncSyncCycle -PolicyType Delta}
+Remove-PSSession $session
