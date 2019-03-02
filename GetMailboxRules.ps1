23e293d6436e9ada2489aa5285713d21f5df1e37
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:10:12 -0800

    This will connect to Office 365, and list all mailbox rules for an individual user.

--- GetMailboxRules.ps1
+++ GetMailboxRules.ps1
@@ -1,4 +1,4 @@
-##This will connect to Office 365, and list all mailbox rules for an individual user.  Helpful when troubleshooting.
+##This will connect to Office 365, and list all mailbox rules for an individual user.  Helpful when troubleshooting
 
 Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
 $EXOSession = New-ExoPSSession
