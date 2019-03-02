46eb84cb0f1de753cb744c2992bdb662cf7b1ae1
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:45:00 -0800

    Forces Active Directory Domain Controllers to sync

--- ForceADReplication.ps1
+++ ForceADReplication.ps1
@@ -4,12 +4,12 @@
 param(
     [ValidateSet("ExtraSuper","Normal")]
     [string]$Mode = 'Normal'
-)  
+)
 
 $DomainControllers = Get-ADDomainController -Filter *
 ForEach ($DC in $DomainControllers.Name) {
     Write-Host "Processing for "$DC -ForegroundColor Green
-    If ($Mode -eq "ExtraSuper") { 
+    If ($Mode -eq "ExtraSuper") {
         REPADMIN /kcc $DC
         REPADMIN /syncall /A /e /q $DC
     }
