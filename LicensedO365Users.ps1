f81c1c28b5210c592cb08daa4a8e9702c6f06c8b
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:33:34 -0800

    Finds and exports Office 365 Licensed users to CSV file

--- LicensedO365Users.ps1
+++ LicensedO365Users.ps1
@@ -1,2 +1,4 @@
+##Finds and exports Office 365 Licensed users to CSV file
+
 Connect-MsolService -Credential $UserCredential
 Get-MsolUser | Where-Object { $_.isLicensed -eq "TRUE" } | Export-Csv c:\users\YOURNAME\desktop\LicensedUsers.csv
