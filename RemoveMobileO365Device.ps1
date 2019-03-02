a2ffa31408d632f9deaaf14346d4a19f8e4cb766
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:42:11 -0800

    Find a mobile device and remove it from Office 365.

--- RemoveMobileO365Device.ps1
+++ RemoveMobileO365Device.ps1
@@ -1,5 +1,5 @@
-##This one is a two parter.  This gets your mobile device information about a particular user, and then you need to manually run the command with the identity, since I don't know how to pull that info :)  ##
-##This is because when you have a user who's left, fired, etc, when you change their password, it can take up to 24 hours for it to process.  That's unacceptable.  However, when you use this, it instantly removes their access. (At least in my experience)##
+###This one is a two parter.  This gets your mobile device information about a particular user, and then you need to manually run the command with the identity, since I don't know how to pull that info :)  ##
+###This is because when you have a user who's left, fired, etc, when you change their password, it can take up to 24 hours for it to process.  That's unacceptable.  However, when you use this, it instantly removes their access. (At least in my experience)##
 
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
