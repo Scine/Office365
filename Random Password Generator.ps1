66a14ec2fa6a87eb677fd64db9598a8cdc59a56b
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:41:20 -0800

    Generates a random password each time it's ran.

--- Random Password Generator.ps1
+++ Random Password Generator.ps1
@@ -2,10 +2,8 @@
 # Change the x,y in the parenthesees right after GeneratePassword
 # x = length in characters
 # y = minimum number of non-alphanumeric characters
-# If you only want alpha-numeric characters, simply use “x.”  
+# If you only want alpha-numeric characters, simply use “x.”
 # As soon as you use “y”, you’ll get anywhere from y to x number of non-alphanumeric characters.
 
 
 $Password = [system.web.security.membership]::GeneratePassword(x,y)
-
-
