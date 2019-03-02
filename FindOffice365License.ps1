9d823777052e752b1523dd44962858c852243bf9
Travis Beehler <tbeehler@gmail.com>
2019-03-01 23:07:08 -0800

    Finding your Office 365 license usage.

---
+++ FindOffice365License.ps1
@@ -0,0 +1,17 @@
+##This will allow you to find the licenses that you have available in Office 365.
+
+#Without 2FA authentication enabled uncomment this section
+
+#$UserCredential = Get-Credential
+#Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session
+
+
+#With 2FA authentication enabled already.  If you don't have this enabled, use the above section on line 6 and DISABLE the next 3 lines below by putting a # at the beginning of each line.
+
+Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
+$EXOSession = New-ExoPSSession
+Import-PSSession $EXOSession
+
+
+Get-AzureADSubscribedSku | Select -Property Sku*,ConsumedUnits -ExpandProperty PrepaidUnits
