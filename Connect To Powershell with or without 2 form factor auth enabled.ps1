1be14ef9b51247508fa771a7ff865e5399a4d125
Travis Beehler <tbeehler@lcgpence.local>
2019-03-06 07:54:16 -0800

    Connect to Office 365 with/without 2 factor authentication.

--- Connect To Powershell with or without 2 form factor auth enabled.ps1
+++ Connect To Powershell with or without 2 form factor auth enabled.ps1
@@ -1,11 +1,17 @@
-#Without 2FA authentication enabled uncomment this section.
+##Note:  This script may be moot at this point since Microsoft
+##has gotten on top of things when it comes to two factor Authentication
+##within scripting now.  However, I wanted to post this regardless just in case.
+
+##Enjoy!
+
+##Without 2FA authentication enabled uncomment this section.
 
 #$UserCredential = Get-Credential
 #Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 #Import-PSSession $Session
 
 
-#With 2FA authentication enabled already.  If you don't have this enabled, use the above section on line 6 and DISABLE the next 3 lines below by putting a # at the beginning of each line.
+##With 2FA authentication enabled already.  If you don't have this enabled, use the above section on line 6 and DISABLE the next 3 lines below by putting a # at the beginning of each line.
 
 Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
 $EXOSession = New-ExoPSSession
