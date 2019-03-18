7e5f22341a715ed2c43190bf92508f89a15c17a1
Travis Beehler <tbeehler@lcgpence.local>
2019-03-18 11:30:05 -0700

    Find all computers and list their last logon date in Active Directory.

---
+++ FindStaleComputers.ps1
@@ -0,0 +1,7 @@
+##This script will search out all computers in your AD, and list them in order of last logon date, newest at the top.  This will help find old computers that aren't active anymore.
+
+##Note, this won't do anything but list them.  You can add an additional bit at the end to disable computers older than X date, but be careful with that.
+
+##If you've enjoyed my script, come on back regularly, to https://github.com/Scine/office365 as I post regularly!
+
+Get-ADComputer -SearchBase "OU=Computers,DC=DOMAIN,DC=SUFFIX" -Filter * -Properties LastLogonDate | sort LastLogonDate -desc | ft Name,LastLogonDate,DistinguishedName
