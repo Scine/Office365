0af3b5fd8bd27b7ea491c1903206ae8cb1dd8ade
Travis Beehler <tbeehler@lcgpence.local>
2019-04-03 10:24:04 -0700

    Enable Multi Factor Authentication for Office 365 account

---
+++ EnableMultiFactorAuthentication.ps1
@@ -0,0 +1,14 @@
+##Note:  This script may be moot at this point since Microsoft
+##has gotten on top of things when it comes to two factor Authentication
+#Import-PSSession $Session
+
+
+##With 2FA authentication enabled already.  If you don't have this enabled, use the above section on line 6 and DISABLE the next 3 lines below by putting a # at the beginning of each line.
+
+Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
+$EXOSession = New-ExoPSSession
+Import-PSSession $EXOSession
+
+$email = read-Host 'Email address:'
+
+Set-MsolUser -UserPrincipalName $email -StrongAuthenticationRequirements $mfa
