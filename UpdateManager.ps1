3fda7be77010bb136d5dde7830f10d3567c7d84c
Travis <Scine@users.noreply.github.com>
2021-04-30 10:53:38 -0700

    Add files via upload

---
+++ UpdateManager.ps1
@@ -0,0 +1,19 @@
+#connecting to the Azure AD
+Connect-AzureAD 
+
+#importing the CSV source which has the changes 
+$data = Import-Csv users.csv
+
+#Iterating through each row in the CSV
+foreach ($row in $data)
+{
+#INFO in the Console
+Write-Host "Updating the user :"  $row.'User Username'    " manager to "  $row.'Manager Username'  -ForegroundColor Yellow 
+
+#Updating the Manager 
+Set-AzureADUserManager -ObjectId (Get-AzureADUser -ObjectId $row.'User Username').Objectid -RefObjectId (Get-AzureADUser -ObjectId $row.'Manager Username').Objectid
+
+#Completion info in the console for the specified row
+Write-Host "Updated." -ForegroundColor Green
+
+}
